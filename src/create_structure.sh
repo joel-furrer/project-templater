708db737016308847707e913c69ab69a64f077c6
joel-furrer <joel.furrer426@gmail.com>
2025-01-19 13:03:06 +0100

    Updated create script, so that you can use parameters

--- src/create_structure.sh
+++ src/create_structure.sh
@@ -1,6 +1,9 @@
+if [ -z "$1" ]; then
+  echo "Usage: $0 <project-name>"
+  exit 1
+fi
 
-echo "Please enter your projects name:"
-read ORDNER_NAME
+ORDNER_NAME="$1"
 
 if [ -d "$ORDNER_NAME" ]; then
   echo "Folder [ $ORDNER_NAME ] already exists."
@@ -13,11 +16,11 @@ echo "Folder [ $ORDNER_NAME ] created successfully."
 cd "$ORDNER_NAME" || exit
 
 mkdir docs logs scripts src tests
+
 touch .env README.md
 
 chmod 644 .env README.md
 chmod 755 docs logs scripts src tests
 
 echo "Project structure created successfully."
-
 
